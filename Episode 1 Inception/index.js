a5895a380bae71cb43d3c722f3afdfa6daf23256
sohammukher <sohammukherjee1996@gmail.com>
2023-06-02 18:19:38 +0530

    Episode 1 : Namaste React Basic React Element

---
+++ Episode 1 Inception/index.js
@@ -0,0 +1,18 @@
+// const heading = document.createElement("h1");
+
+// heading.innerHTML="Hello World";
+
+
+// const root = document.getElementById("root")
+
+// root.appendChild(heading)
+
+//Above JS Code Rewritten in React
+console.log("REACT")
+
+// (TAG_TYPE, {ATTRIBUTE}, CONTENT)
+const heading = React.createElement("h1",{id:"heading"},"Hello World From React!!");
+
+const reactRoot = ReactDOM.createRoot(document.getElementById("root")); // Create Root For React
+
+reactRoot.render(heading); // Rendering the Heading Inside React Root
