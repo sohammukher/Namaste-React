ffa0a10be1c18b0e17454e9dc76ab55201d1f408
sohammukher <sohammukherjee1996@gmail.com>
2023-06-03 20:30:50 +0530

    New Commit

--- Episode 1 Inception/index.js
+++ Episode 1 Inception/index.js
@@ -15,4 +15,28 @@ const heading = React.createElement("h1",{id:"heading"},"Hello World From React!
 
 const reactRoot = ReactDOM.createRoot(document.getElementById("root")); // Create Root For React
 
-reactRoot.render(heading); // Rendering the Heading Inside React Root
+// reactRoot.render(heading); // Rendering the Heading Inside React Root
+
+
+
+// ------------------------------
+
+
+// Building Below Structure
+
+/* <div id = "parent">
+    <div id = "child">
+        <h1 id = "head"></h1>
+
+    </div>
+</div> */
+
+const heading2 = React.createElement("h1",{id:"head1"},"Heading 2")
+
+const parent = React.createElement("div",{id: "parent"}, 
+                React.createElement("div",{id: "child"}, 
+                React.createElement("div",{id: "innerChild"},[heading,heading2])
+                )
+)
+
+reactRoot.render(parent)
